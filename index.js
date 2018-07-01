d1dd6589ceeb4faead34cf9608e6a37d0181f7a1
Mano <manogajapathi@auzmor.com>
2018-07-01 18:38:10 +0530

    Added index route

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+const port = process.env.PORT || 3000;
 var restify = require('restify');
 var jobsData = require('./job-search.json');
+
+function getIndex(req, res, next) {
+  res.send("Backend server is running successfully by Manogajapathi");
+  next();
+}
 
 function getJobDetails(req, res, next) {
   res.json(jobsData);
@@ -7,9 +13,9 @@ function getJobDetails(req, res, next) {
 }
 
 var server = restify.createServer();
+server.get('/', getIndex);
 server.get('/api/jobs', getJobDetails);
-server.head('/api/jobs', getJobDetails);
 
-server.listen(8080, function() {
+server.listen(port, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
