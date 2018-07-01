28f405e5b8e9143826eae600fe63c203f8c96aeb
Mano <manogajapathi@auzmor.com>
2018-07-01 23:04:46 +0530

    Search filter based on Job ID

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # job-search-backend-api
 Restful API build with Restify in Node JS
 
-## Demo URL 
-[https://job-search-backend-api.herokuapp.com/api/jobs](https://job-search-backend-api.herokuapp.com/api/jobs)
+## Demo URL for list of jobs
+[https://job-search-backend-api.herokuapp.com/api/jobs/list](https://job-search-backend-api.herokuapp.com/api/jobs/list)
+
+## Demo URL for particular job [id : 1-25]
+[https://job-search-backend-api.herokuapp.com/api/jobs/:id](https://job-search-backend-api.herokuapp.com/api/jobs/:id)
--- index.js
+++ index.js
@@ -3,16 +3,7 @@ const corsMiddleware = require('restify-cors-middleware')
 var restify = require('restify');
 var jobsData = require('./job-search.json');
 
-function getIndex(req, res, next) {
-  res.send("Backend server is running successfully by Manogajapathi");
-  next();
-}
-
-function getJobDetails(req, res, next) {
-  res.json(jobsData);
-  next();
-}
-
+// CORS for Access-Control
 const cors = corsMiddleware({
   preflightMaxAge: 5, 
   origins: ['*'], 
@@ -20,11 +11,35 @@ const cors = corsMiddleware({
   exposeHeaders: ['API-Token-Expiry']
 })
 
+// Creating sever and setting the routes
 var server = restify.createServer();
 server.pre(cors.preflight)
 server.use(cors.actual)
 server.get('/', getIndex);
-server.get('/api/jobs', getJobDetails);
+server.get('/api/jobs/list', getAllJobDetails);
+server.get('/api/jobs/:id', getJobDetails);
+
+// Server deploy message on home page
+function getIndex(req, res, next) {
+  res.send("Backend server is running successfully by Manogajapathi");
+  next();
+}
+
+// Display list of job details
+function getAllJobDetails(req, res, next) {
+  res.json(jobsData);
+  next();
+}
+
+// Display a particular job details using jobID
+function getJobDetails(req, res, next) {
+  let id = req.params.id;
+  var selectedJob = jobsData.filter(function(i) {
+    return i.jobID == id;
+  }); 
+  res.json(selectedJob);
+  next();
+}
 
 server.listen(port, function() {
   console.log('%s listening at %s', server.name, server.url);
